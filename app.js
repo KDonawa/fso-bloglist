1e35c93c2b86b043012a910c21403d638e821f2d
Kerron Donawa <kerrondonawa@gmail.com>
2022-12-20 17:56:37 -0400

    initial setup

---
+++ app.js
@@ -0,0 +1,19 @@
+const config = require("./utils/config");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const logger = require("./utils/logger");
+const blogRouter = require("./controllers/blogs");
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info("Connected to MongoDB"))
+  .catch((error) => logger.error("error connecting to MongoDB:", error.message));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
+
+module.exports = app;
